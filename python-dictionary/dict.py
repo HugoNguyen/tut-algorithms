47a9864a1c5a7907e93843af58807a4855dfd82a
HugoNguyen <nguyenquoctuan61089@gmail.com>
2021-07-28 18:39:31 +0700

    commit Dictionary

---
+++ python-dictionary/dict.py
@@ -0,0 +1,14 @@
+dict = { 'Joe': 14, 'Adam': 26, 'Emily': 56}
+
+print(dict['Joe']); # O(1)
+
+dict['Joe'] = 15;
+
+print(dict['Joe']);
+
+# dict.clear();
+# del dict
+
+# print(dict.items());
+# print(dict.keys());
+# print(dict.values());
